f8db16518801ba808d1c75e64ea26b92ba1095de
Olga Shtykova <olgashtykova@yahoo.com>
2022-02-06 23:29:42 +0300

    Add properties to Triangle class.

--- Module6/Module6/Triangle.cs
+++ Module6/Module6/Triangle.cs
@@ -1,16 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Module6
+﻿namespace Module6
 {
     public class Triangle
     {
-		public int a;
-		public int b;
-		public int c;
+		private int a;
+		private int b;
+		private int c;
+
+        public int A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                if (value > 0 && b + c > value)
+                    a = value;
+            }
+        }
+
+        public int B 
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                if (value > 0 && a + c > value)
+                    b = value;
+            }
+        }
+		public int C 
+        {
+            get
+            {
+                return c;
+            }
+            set
+            {
+                if (value > 0 && a + b > value)
+                    c = value;
+            }
+        }
 
 		public double Area()
 		{
