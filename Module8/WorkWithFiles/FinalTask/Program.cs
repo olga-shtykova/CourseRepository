016afbcc3e6426fd67e453b5dacc094aea8fc349
Olga Shtykova <olgashtykova@yahoo.com>
2022-02-24 21:32:45 +0300

    Add FinalTask.

---
+++ Module8/WorkWithFiles/FinalTask/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FinalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newDirectory = new DirectoryInfo(@"C:\Users\july2\Desktop\Students");
+            if (!newDirectory.Exists)
+                newDirectory.Create();
+
+            var filePath = @"C:\Users\july2\Desktop\Students.dat";
+
+            var students = new List<Student>();
+
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    var formatter = new BinaryFormatter();
+                    using (var stream = new FileStream(filePath, FileMode.Open))
+                    {
+                        var data = (Student[])formatter.Deserialize(stream);
+                        foreach (var student in data)
+                        {
+                            Console.WriteLine(student.Name);
+                            Console.WriteLine(student.Group);
+                            Console.WriteLine(student.DateOfBirth.ToShortDateString());
+
+                            students.Add(student);
+                        }
+                    }
+
+                    var groups = students.Select(x => x.Group).Distinct().ToList();
+                    foreach (var group in groups)
+                    {
+                        string studentFile = $@"C:\Users\july2\Desktop\Students\{group}.txt";
+
+                        var studentsInGroup = students.Where(s => s.Group == group).ToList();
+
+                        if (!File.Exists(studentFile))
+                        {
+                            using (StreamWriter sw = File.CreateText(studentFile))
+                            {
+                                foreach (var student in studentsInGroup)
+                                {
+                                    sw.WriteLine($"{student.Name}, {student.DateOfBirth}");
+                                }                                    
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"The process failed {e.Message}.");
+                }
+            }
+        }
+    }
+}
---
+++ Module8/WorkWithFiles/FinalTask/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalTask
+{
+    [Serializable]
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
