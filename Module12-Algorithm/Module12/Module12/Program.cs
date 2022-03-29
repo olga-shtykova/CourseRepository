8ef8059f6690b4871caa9672ae3e0c65fa51bf31
Olga Shtykova <olgashtykova@yahoo.com>
2022-03-29 18:18:09 +0300

    Add Module12-Algorithm project.

---
+++ Module12-Algorithm/Module12/Module12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Module12
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+			var users = new List<User>();
+			users.Add(new User { Name = "Таня", IsPremium = true });
+			users.Add(new User { Name = "Виктор", IsPremium = false });
+			users.Add(new User { Name = "Катя", IsPremium = false });
+			users.Add(new User { Name = "Оля", IsPremium = true });
+        }
+
+		static void ShowAds()
+		{
+			Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+			// Остановка на 1 с
+			Thread.Sleep(1000);
+
+			Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+			// Остановка на 2 с
+			Thread.Sleep(2000);
+
+			Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+			// Остановка на 3 с
+			Thread.Sleep(3000);
+		}
+	}
+
+}
---
+++ Module12-Algorithm/Module12/Module12/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Module12
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+    }
+}
